a3d36bd373105cc80c5d314b36861858d431d284
K-Rithz <sothearith.kong@student.cadt.edu.kh>
2026-03-05 10:29:08 +0700

    Ex1 Ex2 Ex3

--- exercise-1/src/App.jsx
+++ exercise-1/src/App.jsx
@@ -1,28 +1,34 @@
 import React from "react";
+
 function App() {
   /* You will need to use a boolean state to manage the weather */
+  const [isRainy, setIsRainy] = React.useState(false);
 
   function onSunClick() {
     // Complete this code when we click on Sunny Time
+    setIsRainy(false);
   }
 
   function onRainClick() {
     // Complete this code when we click on Rain Time
+    setIsRainy(true);
   }
 
   function getTitle() {
     // This function manage the H1 text, depending on the weather
+    return isRainy ? "RAIN TIME!" : "SUN TIME!";
   }
 
   function getBackgroundColor() {
     // This function manage the main class value, depending on the weather
+    return isRainy ? "rainy" : "sunny";
   }
 
   return (
-    <main>
-      <h1>TODO</h1>
-      <button>Sunny Time</button>
-      <button>Rain Time</button>
+    <main className={getBackgroundColor()}>
+      <h1>{getTitle()}</h1>
+      <button onClick={onSunClick}>Sunny Time</button>
+      <button onClick={onRainClick}>Rain Time</button>
     </main>
   );
 }
--- exercise-3/src/App.jsx
+++ exercise-3/src/App.jsx
@@ -1,25 +1,59 @@
 import React from "react";
 
 function App() {
-  /* You will need to use many state to keep the inut values and other needs */
+  /* You will need to use many state to keep the input values and other needs */
+  const [inputA, setInputA] = React.useState("");
+  const [inputB, setInputB] = React.useState("");
+  const [result, setResult] = React.useState("");
+  const [hasError, setHasError] = React.useState(false);
 
   /* You will need some function to handle the key pressed and button events */
+  function onA(event) {
+    setInputA(event.target.value);
+  }
+
+  function onB(event) {
+    setInputB(event.target.value);
+  }
+
+  function isNumeric(value) {
+    const newValue = value.trim();
+    if (newValue === "") {
+      return false;
+    }
+    return !isNaN(newValue);
+  }
+
+  function compute() {
+    if (isNumeric(inputA) && isNumeric(inputB)) {
+      const sum = Number(inputA) + Number(inputB);
+      setResult(String(sum));
+      setHasError(false);
+    } else {
+      setResult("A and B shall be numbers !");
+      setHasError(true);
+    }
+  }
+
+  function checkError() {
+    return hasError ? "error" : "";
+  }
 
   return (
     <main>
       <h1>Calculator</h1>
 
       <label>A =</label>
-      <input onKeyUp={onA} />
+      <input value={inputA} onChange={onA} />
 
       <label>B =</label>
-      <input onKeyUp={onB} />
+      <input value={inputB} onChange={onB} />
 
       <label>A + B =</label>
 
       {/* When Compute buton is clicked, this input display the sum of the 2 numbers, or the error message in RED */}
-      <input disabled />
-      <button>Compute</button>
+      <input disabled value={result} className={checkError()} />
+      <button onClick={compute}>Compute</button>
     </main>
   );
 }
